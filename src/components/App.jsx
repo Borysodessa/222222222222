8c88d62dbf1500e94ee6ec9dd8ba82eef3aeeb07
BorysGull <chayka2021@yahoo.com>
2022-10-26 12:01:40 +0300

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,31 @@
+import {Profile} from "./social-fragment/Profile/Profile";
+import transactions from "../data/transactions.json"
+import user from "../data/user"
+import uploadStats from "data/data.json"
+import {Statistics} from "./Statistics/uploadStatsList/uploadStatsList";
+import {Border} from "./App.styled"
+import friends from "data/friends.json"
+import {FriendList} from "./friendList/FriendList/FriendList";
+import {TransactionHistory} from "./TransactionHistory/TransactionHistory/TransaktionHistory";
 
-import data from "data/data.json"
-import {StatisticTitle} from "./Statistics/statisticsTitle/statisticsTitle"
 
 export const App = () => {
   return (
-    <StatisticTitle title={data} />
+    <>
+      <Border>
+        <Profile profile={user[0]}/>
+      </Border>
+      <Border>
+        <Statistics stats={uploadStats} title={"UPlOAD STATS"}/>
+      </Border>
+      <Border>
+        <FriendList friends={friends}/>
+      </Border>
+      <Border>
+        <TransactionHistory items={transactions}/>
+      </Border>
+    </>
   )
 };
+
+
